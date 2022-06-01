e110c51e1b52823b8ae1d96f6d0cecbc53b95a63
eduardesouza <89279140+eduardesouza@users.noreply.github.com>
2022-05-31 21:09:54 -0300

    att mais exercicios

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Aleatorio from "./components/basicos/Aleatorio";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
+import ListaAlunos from "./components/Repeticao/ListaAlunos";
+import TabelaProdutos from "./components/Repeticao/TabelaProdutos";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (_) => (
@@ -12,6 +16,22 @@ export default (_) => (
     <h1>Fundamentos React</h1>
 
     <div className="Cards">
+      <Card titulo="#07 - Tabela Repetição" color="#3A9AD9">
+        <TabelaProdutos></TabelaProdutos>
+      </Card>
+      
+      <Card titulo="#06 - Repetição" color="#FF4C65">
+        <ListaAlunos></ListaAlunos>
+      </Card>
+
+      <Card titulo="#05 Componente com Filhos" color="#00C8F8">
+        <Familia sobrenome="Silva">
+          <FamiliaMembro nome="Pedro" />
+          <FamiliaMembro nome="Ana" />
+          <FamiliaMembro nome="Gustavo" />
+        </Familia>
+      </Card>
+
       <Card titulo="#04 Desafio Aleatório" color="#FA6900">
         <Aleatorio min={1} max={60} />
       </Card>
